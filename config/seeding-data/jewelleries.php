47d018da7779a7bfe72addf8167686c75bce143b
acwstudio <acwstudio2018@gmail.com>
2024-12-19 03:13:05 +0300

    fixed brooch with garnet

--- config/seeding-data/jewelleries.php
+++ config/seeding-data/jewelleries.php
@@ -61,6 +61,35 @@ return [
             'insert-jewellery' => []
         ],
         [
+            'prcs_metal_property_id' => 5,
+            'jewellery_category_id' => 2,
+            'name' => 'Брошь из золота с гранатами',
+            'description' => '',
+            'part_number' => '740109',
+            'approx_weight' => '1.14 грамм',
+            'insert-jewellery' => [
+                [
+                    'jewellery_id' => 4,
+                    'insert_id' => 13,
+                    'insert_property_id' => 19,
+                ],
+                [
+                    'jewellery_id' => 4,
+                    'insert_id' => '10',
+                    'insert_property_id' => 20,
+                ],
+            ]
+        ],
+        [
+            'prcs_metal_property_id' => 5,
+            'jewellery_category_id' => 2,
+            'name' => 'Брошь-булавка из золота',
+            'description' => '',
+            'part_number' => '40015',
+            'approx_weight' => '0.91 грамм',
+            'insert-jewellery' => []
+        ],
+        [
             'prcs_metal_property_id' => 3,
             'jewellery_category_id' => 6,
             'name' => 'Серебряное наборное кольцо с фианитами',
@@ -69,12 +98,12 @@ return [
             'approx_weight' => '3 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 4,
+                    'jewellery_id' => 6,
                     'insert_id' => '1',
                     'insert_property_id' => 6,
                 ],
                 [
-                    'jewellery_id' => 4,
+                    'jewellery_id' => 6,
                     'insert_id' => '1',
                     'insert_property_id' => 7,
                 ],
@@ -89,12 +118,12 @@ return [
             'approx_weight' => '1.81 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 5,
+                    'jewellery_id' => 7,
                     'insert_id' => '2',
                     'insert_property_id' => 8,
                 ],
                 [
-                    'jewellery_id' => 5,
+                    'jewellery_id' => 7,
                     'insert_id' => '1',
                     'insert_property_id' => 9,
                 ],
@@ -109,12 +138,12 @@ return [
             'approx_weight' => '1.64 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 6,
+                    'jewellery_id' => 8,
                     'insert_id' => '3',
                     'insert_property_id' => 10,
                 ],
                 [
-                    'jewellery_id' => 6,
+                    'jewellery_id' => 8,
                     'insert_id' => '1',
                     'insert_property_id' => 11,
                 ],
@@ -122,19 +151,19 @@ return [
         ],
         [
             'prcs_metal_property_id' => 5,
-            'jewellery_category_id' => 8,
+            'jewellery_category_id' => 9,
             'name' => 'Подвеска из золота с бриллиантами и аметистом',
             'description' => '',
             'part_number' => '73-00124',
             'approx_weight' => '0.74 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 7,
+                    'jewellery_id' => 9,
                     'insert_id' => '4',
                     'insert_property_id' => 12,
                 ],
                 [
-                    'jewellery_id' => 7,
+                    'jewellery_id' => 9,
                     'insert_id' => '5',
                     'insert_property_id' => 13,
                 ],
@@ -149,7 +178,7 @@ return [
             'approx_weight' => '2.31 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 8,
+                    'jewellery_id' => 10,
                     'insert_id' => '6',
                     'insert_property_id' => 14,
                 ]
@@ -164,12 +193,12 @@ return [
             'approx_weight' => '7.7 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 9,
+                    'jewellery_id' => 11,
                     'insert_id' => '7',
                     'insert_property_id' => 15,
                 ],
                 [
-                    'jewellery_id' => 9,
+                    'jewellery_id' => 11,
                     'insert_id' => '8',
                     'insert_property_id' => 16,
                 ],
@@ -184,34 +213,14 @@ return [
             'approx_weight' => '3.6 грамм',
             'insert-jewellery' => [
                 [
-                    'jewellery_id' => 10,
+                    'jewellery_id' => 12,
                     'insert_id' => '5',
                     'insert_property_id' => 17,
                 ],
                 [
-                    'jewellery_id' => 10,
+                    'jewellery_id' => 12,
                     'insert_id' => '9',
                     'insert_property_id' => 18,
-                ],
-            ]
-        ],
-        [
-            'prcs_metal_property_id' => 5,
-            'jewellery_category_id' => 2,
-            'name' => 'Брошь из золота с гранатами',
-            'description' => '',
-            'part_number' => '740109',
-            'approx_weight' => '1.14 грамм',
-            'insert-jewellery' => [
-                [
-                    'jewellery_id' => 11,
-                    'insert_id' => '3',
-                    'insert_property_id' => 19,
-                ],
-                [
-                    'jewellery_id' => 11,
-                    'insert_id' => '10',
-                    'insert_property_id' => 20,
                 ],
             ]
         ],
