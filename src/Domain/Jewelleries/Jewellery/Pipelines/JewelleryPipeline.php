ee9356d865ec5c927a35f9354dadc7152f934e3f
acwstudio <acwstudio2018@gmail.com>
2025-01-21 14:04:41 +0300

    Fixed after crush of Win 11

--- src/Domain/Jewelleries/Jewellery/Pipelines/JewelleryPipeline.php
+++ src/Domain/Jewelleries/Jewellery/Pipelines/JewelleryPipeline.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Domain\Jewelleries\Pipelines\Jewellery;
+namespace Domain\Jewelleries\Jewellery\Pipelines;
 
 use Domain\Shared\AbstractPipeline;
 use Illuminate\Database\Eloquent\Model;
@@ -15,7 +15,7 @@ final class JewelleryPipeline extends AbstractPipeline
         // TODO: Implement store() method.
     }
 
-    public function update(array $data): void
+    public function update(array $data, int $id): void
     {
         // TODO: Implement update() method.
     }
--- src/Domain/Jewelleries/JewelleryCategory/Pipelines/JewelleryCategoryPipeline.php
+++ src/Domain/Jewelleries/JewelleryCategory/Pipelines/JewelleryCategoryPipeline.php
@@ -2,9 +2,26 @@
 
 declare(strict_types=1);
 
-namespace Domain\Jewelleries\Pipelines\JewerellyCategory;
+namespace Domain\Jewelleries\JewelleryCategory\Pipelines;
 
-final class JewelleryCategoryPipeline
+use Domain\Shared\AbstractPipeline;
+use Illuminate\Database\Eloquent\Model;
+
+final class JewelleryCategoryPipeline extends AbstractPipeline
 {
 
+    public function store(array $data): Model
+    {
+        // TODO: Implement store() method.
+    }
+
+    public function update(array $data, int $id): void
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function destroy(int $id): void
+    {
+        // TODO: Implement destroy() method.
+    }
 }
